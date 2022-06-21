cc409574c24af54da01fa4ce13781f8ad18c198f
Callum Buttery <callumbuttery@icloud.com>
2022-06-21 14:09:39 +0100

    Feat: move functions into service
    This way we are adhereing to the 3 layer architecture

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 // controller used to get all things user related
 
@@ -7,18 +8,21 @@ import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   // 1) returns a singular user;
-  @Get()
-  getUsers(): any {
-    return [{ id: 0 }, { name: 'John' }];
+  @Get('all')
+  getAllUsers(): any {
+    return this.usersService.findAll();
   }
 
   // 2) Returns a user by id passed in
   //localhost:3000/users/1
   @Get(':id')
   //need to add the Param decorator to get the id
+  //We always expect id to be a string as it is being pulled from the url
   getUsersById(@Param('id') id: string): any {
-    //return the id we have passed in
-    return [{ id: id }, { name: 'bob' }];
+    // call function inside of service
+    return this.usersService.findById(Number(id));
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  //simulate a database
+  private users: any[] = [
+    { id: 0, name: 'john' },
+    { id: 1, name: 'bob' },
+    { id: 2, name: 'jane' },
+    { id: 3, name: 'joe' },
+    { id: 4, name: 'jill' },
+    { id: 5, name: 'jim' },
+  ];
+
+  findAll(): any[] {
+    return this.users;
+  }
+
+  findById(id: number): any {
+    return this.users.find((user) => user.id === id);
+  }
+}
